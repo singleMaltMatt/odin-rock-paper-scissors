af03f276b444fc28816e5a458eb54778285f0905
singleMaltMatt <mattmatte93@gmail.com>
2022-06-16 16:29:53 +0200

    expanding conditions in playRound function

--- js/script.js
+++ js/script.js
@@ -4,21 +4,36 @@ const computerChoices = [
     'Scissors',
 ]
 
-
 function computerPlay() {
     return computerChoices[Math.floor(Math.random() * computerChoices.length)]
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (computerSelection === 'Paper' && playerSelection === 'rock') {
-        return 'You Lose! Paper beats Rock!'
-    } else if (computerSelection === 'Scissors' && playerSelection === 'rock') { 
-        return 'You Won! good job'
-    } else if (computerSelection === 'Rock' && playerSelection === 'rock') {
-        return 'It\'s a draw!'
+    if (computerSelection === 'Rock' && playerSelection === 'rock') {
+        console.log('The Machine chose ' + computerSelection)
+        return console.warn('It\'s a draw!');
+    } else if (computerSelection === 'Paper' && playerSelection === 'rock') {
+        console.log('The Machine chose ' + computerSelection)
+        return console.warn('You Lose! Paper beats Rock!');
+    } else if (computerSelection === 'Scissors' && playerSelection === 'rock') {
+        console.log('The Machine chose ' + computerSelection)
+        return console.warn('You Won! Rock beats Scissors');
+    } else if (computerSelection === 'Rock' && playerSelection === 'paper') {
+        console.log('The Machine chose ' + computerSelection)
+        return console.warn('You Won! Paper beats Rock!');
+    } else if (computerSelection === 'Paper' && playerSelection === 'paper') {
+        console.log('The Machine chose ' + computerSelection)
+        return console.warn('It\'s a draw!');
+    } else if (computerSelection === 'Scissors' && playerSelection === 'paper') {
+        console.log('The Machine chose ' + computerSelection)
+        return console.warn('You Lose! Scissors beats Paper!');
     }
+
+    
 }
 
-const playerSelection = "rock";
+const playerChoice = prompt('Choose a weapon (Rock, Paper or Scissors)', '');
+const playerSelection = playerChoice.toLowerCase();
+console.log(playerSelection);
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
