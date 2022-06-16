8b5146fe0baee851de0f7c231783f273c2532351
singleMaltMatt <mattmatte93@gmail.com>
2022-06-16 18:48:48 +0200

    added variables and refined playround

--- js/script.js
+++ js/script.js
@@ -4,6 +4,11 @@ const computerChoices = [
     'Scissors',
 ]
 
+let win = 'You Won! ';
+let lose = 'You Lose! ';
+let tie = 'It\'s a draw! ';
+
+
 function computerPlay() {
     return computerChoices[Math.floor(Math.random() * computerChoices.length)]
 }
@@ -11,32 +16,34 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     if (computerSelection === 'Rock' && playerSelection === 'rock') {
         console.log('The Machine chose ' + computerSelection)
-        return console.warn('It\'s a draw!');
+        return console.warn(tie);
     } else if (computerSelection === 'Paper' && playerSelection === 'rock') {
         console.log('The Machine chose ' + computerSelection)
-        return console.warn('You Lose! Paper beats Rock!');
+        return console.warn(lose + 'Paper beats Rock!');
     } else if (computerSelection === 'Scissors' && playerSelection === 'rock') {
         console.log('The Machine chose ' + computerSelection)
-        return console.warn('You Won! Rock beats Scissors');
+        return console.warn( win + 'Rock beats Scissors');
     } else if (computerSelection === 'Rock' && playerSelection === 'paper') {
         console.log('The Machine chose ' + computerSelection)
-        return console.warn('You Won! Paper beats Rock!');
+        return console.warn( win + 'Paper beats Rock!');
     } else if (computerSelection === 'Paper' && playerSelection === 'paper') {
         console.log('The Machine chose ' + computerSelection)
-        return console.warn('It\'s a draw!');
+        return console.warn(tie);
     } else if (computerSelection === 'Scissors' && playerSelection === 'paper') {
         console.log('The Machine chose ' + computerSelection)
-        return console.warn('You Lose! Scissors beats Paper!');
+        return console.warn(lose + 'Scissors beats Paper!');
     } else if (computerSelection === 'Rock' && playerSelection === 'scissors') {
         console.log('The Machine chose ' + computerSelection)
-        return console.warn('You Lose! Rock beats Scissors!');
+        return console.warn(lose + 'Rock beats Scissors!');
     } else if (computerSelection === 'Paper' && playerSelection === 'scissors') {
         console.log('The Machine chose ' + computerSelection)
-        return console.warn('You Won! Scissors beats Paper');
+        return console.warn( win + 'Scissors beats Paper');
     } else if (computerSelection === 'Scissors' && playerSelection === 'scissors') {
         console.log('The Machine chose ' + computerSelection)
-        return console.warn('It\'s a draw!');
-    }
+        return console.warn(tie);
+    } //else {
+        //alert('You can only select Rock, Paper or Scissors!')
+    //}
 }
 
 const playerChoice = prompt('Choose a weapon (Rock, Paper or Scissors)', '');
@@ -47,5 +54,8 @@ console.log(playRound(playerSelection, computerSelection));
 
 function game() { 
     for (let i = 0; i < 5; i++) {
+        playRound();
     }
 }
+
+game();
