0cc7af811c1f1bb4a8b1591468bf6592a873a9e5
singleMaltMatt <mattmatte93@gmail.com>
2022-06-20 18:48:27 +0200

    added some comments

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
+// COMPUTERS CHOICE ARRAY
 const computerChoices = [
     'Rock',
     'Paper',
     'Scissors',
 ]
 
+// WIN LOSE MESSAGES
 let win = 'You Won! ';
 let lose = 'You Lose! ';
 let tie = 'It\'s a draw! ';
 
 
+// FUNCTION TO DETERMINE CHOICE FROM ARRAY
 function computerPlay() {
     return computerChoices[Math.floor(Math.random() * computerChoices.length)]
 }
 
+// FUNCTION TO DETERMINE WINNER BETWEEN COMPUTER AND HUMAN
 function playRound(playerSelection, computerSelection) {
     if (computerSelection === 'Rock' && playerSelection === 'rock') {
         console.log('The Machine chose ' + computerSelection)
@@ -46,16 +50,17 @@ function playRound(playerSelection, computerSelection) {
     //}
 }
 
+// VARIABLES TO START GAME
 const playerChoice = prompt('Choose a weapon (Rock, Paper or Scissors)', '');
 const playerSelection = playerChoice.toLowerCase();
 console.log(playerSelection);
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
 
+// FUNCTION TO RUN GAME AND LOOP ROUNDS UNTIL WINNER
 function game() { 
     for (let i = 0; i < 5; i++) {
         playRound();
     }
 }
 
-game();
